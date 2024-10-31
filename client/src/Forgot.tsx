8610445c0dd5fb215154c5011bae01a8cfedf618
NikitaB0br <bobrovskii.nikita1471@gmail.com>
2024-10-31 13:14:27 +0300

    Refactor code

--- client/src/Forgot.tsx
+++ client/src/Forgot.tsx
@@ -4,70 +4,76 @@ import eyeOpenIcon from './assets/eye-svgrepo-com.svg';
 import eyeClosedIcon from './assets/eye-closed-svgrepo-com.svg';
 
 const Forgot: React.FC = () => {
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
-    };
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
 
-    const handleConfirmPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setConfirmPassword(e.target.value);
-    };
+  const handleConfirmPasswordChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setConfirmPassword(e.target.value);
+  };
 
-    const toggleShowPassword = () => {
-        setShowPassword(prevState => !prevState);
-    };
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
 
-    const isPasswordMatch = password === confirmPassword && password.length > 0;
-    const showError = (password.length > 0 || confirmPassword.length > 0) && !isPasswordMatch;
+  const isPasswordMatch = password === confirmPassword && password.length > 0;
+  const showError =
+    (password.length > 0 || confirmPassword.length > 0) && !isPasswordMatch;
 
-    return (
-        <div className="forgotContainer">
-            <div className="forgotForm">
-                <h1 className="forgotTitle">Смена пароля</h1>
-                <div className="forgotContent">
-                    <p className="forgotLabel">Введите новый пароль</p>
-                    <div className="inputWrapper">
-                        <input
-                            type={showPassword ? 'text' : 'password'}
-                            className="forgotInput"
-                            value={password}
-                            onChange={handlePasswordChange}
-                        />
-                        <img
-                            src={showPassword ? eyeOpenIcon : eyeClosedIcon}
-                            alt="Toggle password visibility"
-                            className="togglePassword"
-                            onClick={toggleShowPassword}
-                        />
-                    </div>
-                    <p className="forgotLabel">Повторите пароль</p>
-                    <div className="inputWrapper">
-                        <input
-                            type={showPassword ? 'text' : 'password'}
-                            className="forgotInput"
-                            value={confirmPassword}
-                            onChange={handleConfirmPasswordChange}
-                        />
-                        <img
-                            src={showPassword ? eyeOpenIcon : eyeClosedIcon}
-                            alt="Toggle password visibility"
-                            className="togglePassword"
-                            onClick={toggleShowPassword}
-                        />
-                    </div>
-                    <p className={`errorMessage ${showError ? '' : 'hidden'}`}>
-                        Пароли не совпадают
-                    </p>
-                </div>
-                <button className={`confirmButton ${!isPasswordMatch ? 'disabled' : ''}`} disabled={!isPasswordMatch}>
-                    Подтвердить
-                </button>
-            </div>
+  return (
+    <div className="forgotContainer">
+      <div className="forgotForm">
+        <h1 className="forgotTitle">Смена пароля</h1>
+        <div className="forgotContent">
+          <p className="forgotLabel">Введите новый пароль</p>
+          <div className="inputWrapper">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              className="forgotInput"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <img
+              src={showPassword ? eyeOpenIcon : eyeClosedIcon}
+              alt="Toggle password visibility"
+              className="togglePassword"
+              onClick={toggleShowPassword}
+            />
+          </div>
+          <p className="forgotLabel">Повторите пароль</p>
+          <div className="inputWrapper">
+            <input
+              type={showPassword ? 'text' : 'password'}
+              className="forgotInput"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+            />
+            <img
+              src={showPassword ? eyeOpenIcon : eyeClosedIcon}
+              alt="Toggle password visibility"
+              className="togglePassword"
+              onClick={toggleShowPassword}
+            />
+          </div>
+          <p className={`errorMessage ${showError ? '' : 'hidden'}`}>
+            Пароли не совпадают
+          </p>
         </div>
-    );
+        <button
+          className={`confirmButton ${!isPasswordMatch ? 'disabled' : ''}`}
+          disabled={!isPasswordMatch}
+        >
+          Подтвердить
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Forgot;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,37 +1,34 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
-import App from './App.tsx'
-import './index.scss'
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-import Authorization from "./Authorization.tsx";
-import Forgot from "./Forgot.tsx";
-import EmailConfirm from "./EmailConfirm.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.scss';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Authorization from './Authorization.tsx';
+import Forgot from './Forgot.tsx';
+import EmailConfirm from './EmailConfirm.tsx';
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-    },
-    {
-        path: '/auth',
-        element: <Authorization/>
-    },
-    {
-      path: '/forgot',
-      element: <Forgot/>
-    },
-    {
-      path: '/emailConfirm',
-      element: <EmailConfirm/>
-    },
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/auth',
+    element: <Authorization />,
+  },
+  {
+    path: '/forgot',
+    element: <Forgot />,
+  },
+  {
+    path: '/emailConfirm',
+    element: <EmailConfirm />,
+  },
 ]);
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <RouterProvider router={router}/>
-        {/*<App />*/}
-    </StrictMode>,
-)
+  <StrictMode>
+    <RouterProvider router={router} />
+    {/*<App />*/}
+  </StrictMode>,
+);

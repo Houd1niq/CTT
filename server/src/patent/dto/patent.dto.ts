e9054e5ecf07fc8e4ca6a166b3ba5ae838371eb7
Zaharov Semeon <75267187+Houd1niq@users.noreply.github.com>
2024-11-19 23:33:54 +0300

    back 80%

--- server/src/patent/dto/patent.dto.ts
+++ server/src/patent/dto/patent.dto.ts
@@ -1,42 +1,49 @@
 import {
-  IsBoolean,
-  IsInt,
-  IsNotEmpty,
-  IsString,
-  Matches,
+    IsNotEmpty,
+    IsString,
+    Matches,
 } from "class-validator";
 
 export class CreatePatentDto {
-  @IsString()
-  @IsNotEmpty()
-  name: string;
+    @IsString()
+    @IsNotEmpty()
+    name: string;
 
-  @IsString()
-  @Matches(/^\d{2}\.\d{2}\.\d{4}$/, {
-    message: "Дата должна быть в формате dd.mm.yyyy",
-  })
-  @IsNotEmpty()
-  dateOfRegistration: string;
+    @IsString()
+    @IsNotEmpty()
+    patentNumber: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @Matches(/^\d{2}\.\d{2}\.\d{4}$/, {
-    message: "Дата должна быть в формате dd.mm.yyyy",
-  })
-  dateOfExpiration: string;
+    @IsString()
+    @Matches(/^\d{2}\.\d{2}\.\d{4}$/, {
+        message: "Дата должна быть в формате dd.mm.yyyy",
+    })
+    @IsNotEmpty()
+    dateOfRegistration: string;
 
-  @IsString()
-  @IsNotEmpty()
-  contact: string;
+    @IsString()
+    @IsNotEmpty()
+    @Matches(/^\d{2}\.\d{2}\.\d{4}$/, {
+        message: "Дата должна быть в формате dd.mm.yyyy",
+    })
+    dateOfExpiration: string;
 
-  @IsBoolean()
-  isPrivate?: boolean;
+    @IsString()
+    @IsNotEmpty()
+    contact: string;
 
-  @IsInt()
-  @IsNotEmpty()
-  patentTypeId: number;
+    @IsString()
+    @Matches(/false|true/, {
+        message: "Значение должно быть 'true' или 'false'",
+    })
+    isPrivate?: string;
 
-  @IsInt()
-  @IsNotEmpty()
-  technologyFieldId: number;
+    @IsString()
+    @IsNotEmpty()
+    patentTypeId: string;
+
+    @IsString()
+    @IsNotEmpty()
+    technologyFieldId: string;
+
+    patentFile: any;
 }
--- server/src/patent/patent.module.ts
+++ server/src/patent/patent.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { PatentService } from './patent.service';
-import { PatentController } from './patent.controller';
+import {Module} from '@nestjs/common';
+import {PatentService} from './patent.service';
+import {PatentController} from './patent.controller';
+import {PatentSearchService} from "./patentSearch.service";
+import {SearchModule} from "../search-module/search-module.module";
 
 @Module({
-  providers: [PatentService],
-  controllers: [PatentController]
+    providers: [PatentService, PatentSearchService],
+    controllers: [PatentController],
+    imports: [SearchModule]
 })
-export class PatentModule {}
+export class PatentModule {
+}

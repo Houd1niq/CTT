17a1e5e46500025acf197ce0253c7c1bde6694f4
Zaharov Semeon <75267187+Houd1niq@users.noreply.github.com>
2025-01-24 14:07:16 +0300

    Докер

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,11 +1,10 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import {Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
+import {PrismaClient} from '@prisma/client';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+  implements OnModuleInit, OnModuleDestroy {
   constructor() {
     super({
       datasources: {
@@ -16,8 +15,31 @@ export class PrismaService
     });
   }
 
+  private async retryConnect(maxRetries = 5, delay = 5000): Promise<void> {
+    let retries = 0;
+
+    while (retries < maxRetries) {
+      try {
+        console.log(`Attempting to connect to the database... (Attempt ${retries + 1}/${maxRetries})`);
+        await this.$connect();
+        console.log('Database connected successfully!');
+        break;
+      } catch (error) {
+        retries++;
+        console.error(`Database connection failed. Retrying in ${delay / 1000}s...`, error);
+
+        if (retries >= maxRetries) {
+          console.error('Max retries reached. Unable to connect to the database.');
+          throw error;
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, delay));
+      }
+    }
+  }
+
   async onModuleInit() {
-    await this.$connect();
+    await this.retryConnect();
   }
 
   async onModuleDestroy() {
